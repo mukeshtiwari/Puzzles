f79d024e248e591a34cf2266b728966fbc01a284
Mukesh Tiwari <mukeshtiwari.iiitm@gmail.com>
2013-08-19 18:39:17 +0530

    python title downloader

---
+++ TitleDownloader.py
@@ -0,0 +1,37 @@
+import urllib2
+import socket
+import Queue
+import thread
+from bs4 import BeautifulSoup
+
+class Downloader():
+	socket.setdefaulttimeout ( 10 )
+
+	def __init__( self ):
+			self.q = Queue.Queue( 100 )
+
+	def downloadurl( self ) :
+		try:	
+			while True :
+				url = self.q.get( )
+				data = urllib2.urlopen ( url ) 
+				parsed = BeautifulSoup ( data.read() , 'lxml' )
+				print parsed.find( 'title' )
+		except  :
+			print 'error occured in download url' 
+
+
+	def createurl ( self  ) : 
+		try : 
+			while True :
+				self.q.put('http://www.aol.com' )
+		
+		except :
+			print 'error occured in create url'
+
+if __name__== '__main__':
+	u = Downloader()
+	thread.start_new_thread ( u.createurl , () )
+	thread.start_new_thread ( u.downloadurl , () )
+	while True : pass
+
