3fcd93f5b9c87905399e1f2ad773c7b5ba9ca6c4
Mukesh Tiwari <mukeshtiwari.iiitm@gmail.com>
2013-08-21 00:40:44 +0530

    regular expression

--- TitleDownloader.py
+++ TitleDownloader.py
@@ -2,6 +2,7 @@ import urllib2
 import socket
 import Queue
 import thread
+import re
 from bs4 import BeautifulSoup
 from random import randrange
 
@@ -9,38 +10,41 @@ class Downloader():
 
 	def __init__( self ):
 			self.q = Queue.Queue( 100 )
-			self.notvalid = [ 10, 127, 169, 172, 192]
 			socket.setdefaulttimeout( 10 ) 
 
 	def downloadurl( self ) :	
 			while True :
 				try :
 					url = self.q.get( )
-					data = urllib2.urlopen ( url ) 
-					parsed = BeautifulSoup ( data.read() , 'lxml' )
-					print parsed.find( 'title' )
-				except  :
+					data = urllib2.urlopen ( url )
+					regex = re.compile('<title>(.*?)</title>' , re.IGNORECASE)
+					title = regex.search(data.read())
+					print ', '.join ( [ url , title.group(1) ] )
+					#parsed = BeautifulSoup ( data.read() , 'lxml' )
+					#print  ', '.join ( [ url , parsed.find( 'title' ).renderContents() ] )
+					data.close()
+				except :
 					print 'error occured in download url' 
 
 
 	def createurl ( self  ) : 
-			#create the url here
-			while True :
-				try :
-					self.q.put('http://www.aol.com' )
-					self.q.put('http://www.facebook.com')
-					self.q.put('http://www.yahoo.com')
-					self.q.put('http://www.icicibank.com')
-					self.q.put('http://www.apple.com')
-					self.q.put('http://173.200.40.143')
-					first = randrange( 1, 256 )
-					while first in self.notvalid : 
-						first = randrange( 1, 256 ) 
-					ip = '.'.join([ str( first ), str( randrange( 1, 256 ) ), str( randrange( 1, 256 ) ), str( randrange( 1, 256 ) ) ] )
-					#print ip 
-					self.q.put('http://ip')
-				except :
-					print 'error occured in create url'
+			#This reading line by line is good but it's slow.
+			#with open('top-1m.csv', 'r') as file:
+				#for line in iter(file.readline, ''):
+				#	self.q.put (  ''.join ( [ 'http://www.' , line.split(',')[1] ] ) )
+
+			#Reading data in chunks is fast but we can miss some sites due to reading the data in chunks( It's worth missing because reading is very fast)
+			with open('top-1m.csv', 'r') as file:
+				prefix = ''
+				for lines in iter(lambda:file.read(1024) , ''):
+					l = lines.split('\n')
+					n = len ( l )
+					l[0] = ''.join( [ prefix , l[0] ] )
+					for i in xrange ( n - 1 ) : self.q.put ( ''.join ( [ 'http://www.', l[i].split(',')[1] ] )  )
+					prefix = l[n-1]
+
+			
+
 
 if __name__== '__main__':
 	u = Downloader()
