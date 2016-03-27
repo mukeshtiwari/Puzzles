cf841e6cf7a4409aab41f41d0e552c3e0d5211dd
Mukesh Tiwari <mukeshtiwari.iiitm@gmail.com>
2016-03-28 05:16:47 +0530

    csp solution

---
+++ constraint-prog/doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to constraint-prog
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ constraint-prog/test/constraint_prog/core_test.clj
@@ -0,0 +1,7 @@
+(ns constraint-prog.core-test
+  (:require [clojure.test :refer :all]
+            [constraint-prog.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
