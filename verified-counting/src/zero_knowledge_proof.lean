6f6f5db7633f537ec83dc345ebffffbb6eb9f49d
mukesh tiwari <mukeshtiwari.iiitm@gmail.com>
2020-12-06 15:17:48 +1100

    I needed to state soundness, completeness, and honest verifier zkp lemma

--- verified-counting/src/zero_knowledge_proof.lean
+++ verified-counting/src/zero_knowledge_proof.lean
@@ -36,16 +36,43 @@ We can construct a sigma protocol (discrete logarithm).
 we can always construct a valid certificate. Moreover, we will prove this 
 formally that this function always constructs a valid certificate which checks out
 -/
-def construct_sigma_protocol (w x : zmod q) (h : zmod p) (H₁ : h = g^x.val) :  
-    Σ (t c s : zmod q), zkp_transcript p q g hash w x h H₁ (response t c s)  := 
-    let t : zmod q := g^w.val in 
-    let c : zmod q := hash [g, h, t] in 
-    let s : zmod q := w + c * x in 
-    ⟨t, c, s, response_step t c s rfl (challenge_step t c rfl (commitment_step t rfl)) ⟩
+def construct_nzkp_certificate (w x : zmod q) (h : zmod p) (H : h = g^x.val) :  
+    Σ (t c s : zmod q), zkp_transcript p q g hash w x h H (response t c s)  := 
+      let t : zmod q := g^w.val in 
+      let c : zmod q := hash [g, h, t] in 
+      let s : zmod q := w + c * x in 
+      ⟨t, c, s, response_step t c s rfl (challenge_step t c rfl (commitment_step t rfl)) ⟩
+
+/- a given certificate is a valid one -/
+def accept_nzkp_certificate  (w x : zmod q) (h : zmod p) (H : h = g^x.val)
+  (t c s : zmod q) (Hnzkp : zkp_transcript p q g hash w x h H (response t c s)) := 
+  g^s.val = t * h^c.val 
+
+/- invalid transcript -/
+def reject_nzkp_certificate  (w x : zmod q) (h : zmod p) (H : h = g^x.val)
+  (t c s : zmod q) (Hnzkp : zkp_transcript p q g hash w x h H (response t c s)) := 
+  g^s.val ≠ t * h^c.val 
 
 
+/- 
+Proof that the construct_nzkp_certificate function always constructs 
+a valid certificate. Each valid certificate always checks out. 
+Completeness 
+-/
+theorem proof_of_completeness : ∀ (w x : zmod q) (h : zmod p) (H₀ : h = g^x.val) cert 
+  (H₁ : cert = construct_nzkp_certificate p q g hash w x h H₀), 
+  accept_nzkp_certificate p q g hash w x h H₀ cert.1 cert.2.1 cert.2.2.1 cert.2.2.2 = true :=
+  begin 
+    sorry
+  end 
+
+/- If you give me two valid ceritificate for same randomness, then I can extract a witness x : Soundenss  -/
+lemma proof_of_soundness : ∀ (w x : zmod q) (h : zmod p) (H₀ : h = g^x.val) cert₁ cert₂ 
+  (H₁ : cert₁ = construct_nzkp_certificate p q g hash w x h H₀)
+  (H₂ : cert₂ = construct_nzkp_certificate p q g hash w x h H₀), 
+
+  
 
 /- -/
 
 end nzkp
-#eval nzkp.construct_sigma_protocol
